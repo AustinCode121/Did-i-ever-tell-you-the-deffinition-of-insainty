a7bad4e386fc76e7a3096d2a2699f35ea8a9cb2c
AustinCode121 <73318569+AustinCode121@users.noreply.github.com>
2021-12-01 09:21:10 -0500

    Create insainty.php

---
+++ insainty.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+echo "<br>";
+echo "Did I ever tell you what the definition of insanity is? Insanity";
+echo "<br>";
+echo "is doing the exact... same fucking thing... over and over again";
+echo "<br>";
+echo "expecting... shit to change... That. Is. Crazy. The first time
+<br>somebody";
+echo "<br>";
+echo "told me that, I dunno, I thought they were bullshitting me, so, I <br>shot him.";
+echo "<br>";
+echo "The thing is... He was right. And then I started seeing, <br> everywhere I looked,";
+echo "<br>";
+echo "everywhere I looked all these fucking pricks, everywhere I looked,";
+echo "<br>";
+echo "doing the exact same fucking thing... over and over and over and <br> over again";
+echo "<br>";
+echo "thinking 'this time is gonna be different'";
+echo "<br>";
+echo "no, no, no please... This time is gonna be different,";
+echo "<br>";
+echo " I'm sorry, I don't like... The way...";
+echo "<br>";
+echo " [Punches crate aside violently. His agitation towards the player <br> character is visibly growing]";
+echo "<br>";
+echo " ... you are looking at me... Okay, Do you have a fucking problem <br>in your head,";
+echo "<br>";
+echo " do you think I am bullshitting you, do you think I am lying?";
+echo "<br>";
+echo "Fuck you! Okay? Fuck you!... ";
+echo "<br>";
+echo"It's okay, man. I'm gonna chill, hermano. I'm gonna chill... The <br>thing is...";
+echo "<br>";
+echo " Alright, the thing is I killed you once already... and it's not <br>like I am fucking crazy.";
+echo "<br>";
+echo " It's okay... It's like water under the bridge.";
+echo "<br>";
+echo "Did I ever tell you the definition... of insanity?";
+echo "<br>";
+
+           
+?> 
+
+</body>
+</html>
